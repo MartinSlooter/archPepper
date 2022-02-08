a15ea08164c2ca5e47b1a16012b133bc37d131b2
Martin Slooter <80220595+MartinSlooter@users.noreply.github.com>
2022-02-08 19:38:49 +0100

    Create setup-system.sh

---
+++ setup-system.sh
@@ -0,0 +1,3 @@
+sudo pacman -Sy bspwm sxhkd rxvt-unicode xterm xorg xorg-randr lightdm lightdm-gtk-greeter nitrogen git pcmanfm
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo yay -S polybar
