be75d404136a40132b71c25555de3b5ef2f82e08
Martin Slooter <80220595+MartinSlooter@users.noreply.github.com>
2022-02-08 20:00:04 +0100

    Update setup-system.sh

--- setup-system.sh
+++ setup-system.sh
@@ -1,3 +1,3 @@
-sudo pacman -Sy bspwm sxhkd rxvt-unicode xterm xorg-randr lightdm lightdm-gtk-greeter nitrogen git pcmanfm
+sudo pacman -Sy bspwm sxhkd rxvt-unicode xterm xorg-randr lightdm lightdm-gtk-greeter nitrogen git pcmanfm blueman
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -S polybar
